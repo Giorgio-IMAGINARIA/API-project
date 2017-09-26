bff19850843ae333dc5d931ccef7b6ce35798807
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2017-09-27 00:56:07 +0100

    auth messages added

--- routes/auth.js
+++ routes/auth.js
@@ -8,16 +8,22 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 router.post('/', function (req, res, next) {
-    if (count(req.body)===2){
-        console.log('correct number of properties');
-        console.log('the number of keys is: ', count(req.body));
-        console.log('req.body: ', req.body);
-        res.json({ response: true });
+    if (count(req.body) === 2) {
+        // console.log('correct number of properties');
+        // console.log('the number of keys is: ', count(req.body));
+        // console.log('req.body: ', req.body);
+        // res.json({ response: true });
+        res.status(200).send({
+            message: 'Accepted auth POST request',
+            objectProcessed: req.body
+        });
+
     } else {
-        console.log('the number of properties is not correct');
-        console.log('the number of keys is: ', count(req.body));
-        console.log('req.body: ', req.body);
-        res.json({ response: false });
+        // console.log('the number of properties is not correct');
+        // console.log('the number of keys is: ', count(req.body));
+        // console.log('req.body: ', req.body);
+        // res.json({ response: false });
+        res.status(400).send({ message: 'Malformed auth POST request' });
     };
 });
 
