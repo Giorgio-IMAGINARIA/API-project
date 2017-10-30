471604372616af41245380b13b8a329eb74ecd8b
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2017-10-30 00:36:35 +0000

    small changes

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const { check } = require('express-validator/check');
-const { sanitize } = require('express-validator/filter');
 
 // Require controller modules
 var accounts_controller = require('../controllers/accountsController');
